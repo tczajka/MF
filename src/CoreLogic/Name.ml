8bd0d0737ecc95d7cca533a689ab6aa36b0e108b
Tomek Czajka <tczajka@gmail.com>
2017-05-20 00:40:38 -0700

    Small changes to Name

--- src/CoreLogic/Name.ml
+++ src/CoreLogic/Name.ml
@@ -1,59 +1,60 @@
-(**
+(*
  * Names of various things: type constructors, constants, variables.
  *
- * @author Tomek Czajka, 2017
+ * Author: Tomek Czajka, 2017.
  *)
 
-(**
+(*
  * Operator associativity.
  *
  * Determines how operators of same precedence associate.
  *)
 type operator_associativity =
+  (* Associate to the left, e.g. "a + b + c" means "(a + b) + c". *)
   | LeftAssociativity
-    (** Associate to the left, e.g. "a + b + c" means "(a + b) + c". *)
+  (* Associate to the right, e.g. "a -> b -> c" means "a -> (b -> c)" *)
   | RightAssociativity
-    (** Associate to the right, e.g. "a -> b -> c" means "a -> (b -> c)" *)
+  (* Do not associate to the left or to the right, e.g. "a < b < c" does not mean anything. *)
   | NoAssociativity
 
-(**
+(*
  * Operator precedence.
  *
- * Operators with higher precedence is applied first.
+ * Operators with higher precedence are applied first.
  *
  * For instance, if the precedence of "*" is higher than the precedence of "+",
  * "a + b * c" means "a + (b * c)".
  *)
 type operator_precedence = int
 
-(**
- * Describes how to parse a given element.
+(*
+ * Describes how a token is used in a term.
  *)
 type token_syntax =
-  | RegularSyntax
-    (** No special parsing rules apply. *)
-  | OperatorSyntax of operator_precedence * operator_associativity
-    (**
-     * Parse as an infix operator.
-     *
-     * For instance, "2 + 3" means "(+) 2 3" rather than "2 (+) 3".
-     *
-     * But "(+) 2 3" is also OK. Do not parse as an operator
-     * if enclosed in parantheses.
-     *)
-  | BinderSyntax
-    (**
-     * Parse as a binder.
-     *
-     * For instance, if "all" is a binder:
-     * "all x . x > 3" means "all (fun x . x > 3)", and
-     * "all x y . x > y" means "all x . all y . x > y".
-     *
-     * But "(all) (fun x . x > 3)" is also OK.
-     * Do not parse as a binder if enclosed in parantheses.
-     *)
+  (* No special rules apply. *)
+  | RegularToken
+  (**
+   * Parse as an infix operator.
+   *
+   * For instance, "2 + 3" means "(+) 2 3" rather than "2 (+) 3".
+   *
+   * But "(+) 2 3" is also OK. Do not parse as an operator
+   * if enclosed in parantheses.
+   *)
+  | OperatorToken of operator_precedence * operator_associativity
+  (*
+   * Parse as a binder.
+   *
+   * For instance, if "all" is a binder:
+   * "all x . x > 3" means "all (fun x . x > 3)", and
+   * "all x y . x > y" means "all x . all y . x > y".
+   *
+   * But "(all) (fun x . x > 3)" is also OK.
+   * Do not parse as a binder if enclosed in parantheses.
+   *)
+  | BinderToken
 
-(**
+(*
  * Identifiers of types, constants, variables.
  *
  * HACK!!!
@@ -68,4 +69,4 @@ type identifier = string
 (**
  * Names of things that can appear in terms.
  *)
-type name = identifier * token_syntax
+type name = Name of identifier * token_syntax
